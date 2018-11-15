830299520aa8d46dfbbe284a52bf4b71b046e75f
James Mckay <james@mckay.co.uk>
2018-11-15 18:42:34 +0000

    WORKING

--- legacy/ShaderProgram.cpp
+++ legacy/ShaderProgram.cpp
@@ -89,7 +89,39 @@ ShaderProgram::ShaderProgram()
 	glDetachShader(id, fragmentShaderId);
 	glDeleteShader(fragmentShaderId);
 }
+void ShaderProgram::printShaderInfoLog(GLuint obj)
+{
+	int infologLength = 0;
+	int charsWritten = 0;
+	char *infoLog;
 
+	glGetShaderiv(obj, GL_INFO_LOG_LENGTH, &infologLength);
+
+	if (infologLength > 0)
+	{
+		infoLog = (char *)malloc(infologLength);
+		glGetShaderInfoLog(obj, infologLength, &charsWritten, infoLog);
+		printf("%s\n", infoLog);
+		free(infoLog);
+	}
+}
+
+void ShaderProgram::printProgramInfoLog(GLuint obj)
+{
+	int infologLength = 0;
+	int charsWritten = 0;
+	char *infoLog;
+
+	glGetProgramiv(obj, GL_INFO_LOG_LENGTH, &infologLength);
+
+	if (infologLength > 0)
+	{
+		infoLog = (char *)malloc(infologLength);
+		glGetProgramInfoLog(obj, infologLength, &charsWritten, infoLog);
+		printf("%s\n", infoLog);
+		free(infoLog);
+	}
+}
 
 
 ShaderProgram::ShaderProgram(std::string vert, std::string frag)
